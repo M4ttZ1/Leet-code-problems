85c2e6abf4923c9214ada61ccf3a43a5a1d9b9d4
Matthew Zimmerman <141796088+M4ttZ1@users.noreply.github.com>
2024-01-24 11:33:52 -0700

    Time: 68 ms (55.58%) | Memory: 57.1 MB (5.96%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let low = 0;
+    let high = height.length - 1;
+    let ans = 0;
+
+    while (low <= high) {
+        const length = Math.min(height[low], height[high]);
+        const breadth = high - low;
+        const area = length * breadth;
+        ans = Math.max(ans, area);
+
+        if (height[low] < height[high]) low++;
+        else high--;
+    }
+    return ans;
+
+};

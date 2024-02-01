d0500d6ae751c8ce03b7458740a74009ff1a9c32
Matthew Zimmerman <141796088+M4ttZ1@users.noreply.github.com>
2024-02-01 06:10:22 -0700

    Time: 50 ms (82.00%) | Memory: 50 MB (15.33%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.ts
+++ 367-valid-perfect-square/valid-perfect-square.ts
@@ -1,3 +1,23 @@
-function isPerfectSquare(num: number): boolean {
-    return num % Math.sqrt(num) === 0
-};
+
+
+function isPerfectSquare(num: number) {
+    function squareNum(mid: number) {
+        return mid * mid;
+    }
+    let low = 1;
+    let high = num;
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+        const sqrt = squareNum(mid);
+        if (sqrt === num) {
+            return true;
+        } else if (sqrt > num) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return false;
+}
